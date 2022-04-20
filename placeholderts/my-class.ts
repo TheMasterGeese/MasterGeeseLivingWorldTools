8aa37741bc99cfeb0ab50914b1b5d687ecbbc097
TheMasterGeese <themastergeese@gmail.com>
2022-04-20 17:59:21 -0400

    remove unneeded files

--- placeholderts/my-class.ts
+++
@@ -1,8 +0,0 @@
-export default class MyClass {
-	static async showExampleDialog() {
-		await Dialog.confirm({
-			title: game.i18n.localize('MODULE_PACKAGE_NAME.ExampleDialog_Title'),
-			content: game.i18n.localize('MODULE_PACKAGE_NAME.ExampleDialog_Content')
-		})
-	}
-}
