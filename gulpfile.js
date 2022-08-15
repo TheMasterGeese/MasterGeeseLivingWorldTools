e74c894cfb6cec99b301bcbe92f8a32b04ebe810
MasterGeese <themastergeese@gmail.com>
2022-08-14 21:04:52 -0400

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -203,6 +203,9 @@ function test() {
 		({ stdout, stderr } = await exec(`docker-compose down`));
 		console.log(stdout);
 		console.log(stderr);
+		({ stdout, stderr } = await exec(`docker logs -f ${DOCKER_CONTAINER}`));
+		console.log(stdout);
+		console.log(stderr);
 	}
 }
 
